c337fd94a05304ec685bab5b58804378f6e19e3c
nk44ukrnet <56731417+nk44ukrnet@users.noreply.github.com>
2022-06-07 15:08:27 +0300

    wro website update

--- js/js.js
+++ js/js.js
@@ -1,22 +1,53 @@
 (function () {
+    //hero swiper
+    try {
+        var swiper = new Swiper(".mySwiper1", {
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+        });
+    } catch (e) {
+
+    }
+
+    try {
+        var swiper = new Swiper(".mySwiper2", {
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+        });
+    } catch (e) {
+
+    }
+
     //fetch news from robotica
-    function newsItemTemplate({link, title, img, content}) {
+    function newsItemTemplate({img, content}) {
         return `
-        <div class="news__item">
-                        <h3 class="news__title"><a href="${link}" target="_blank">${title}</a></h3>
-                        <div class="news__content">
-                            <a href="${link}" target="_blank" class="news__image">
-                                <img src="${img}" alt="${title}" loading="lazy">
-                            </a>
+        <div class="swiper-slide">
                             <div class="news__text">
                                 ${content}
                             </div>
+                            <div class="news__image">
+                                <img src="${img}" alt="Image" loading="lazy">
+                            </div>
                         </div>
-                    </div>
         `;
     }
-    fetch('https://robotica.in.ua/wp-json/wp/v2/posts?categories=8&per_page=3')
-        .then(res=>res.json())
-        .then(data=>console.log(data))
-        .catch(err=>console.log(err.message));
+
+    fetch('https://robotica.in.ua/wp-json/wp/v2/posts?categories=8&per_page=4')
+        .then(res => res.json())
+        .then(data => {
+            let latestNews = document.getElementById('latestNews');
+            if (latestNews) {
+                data.forEach(item => {
+                    let img = item.fimg_url || 'https://robotica.in.ua/wp-content/uploads/2019/05/robotica_new.jpg',
+                        text = item.excerpt.rendered;
+                    latestNews.innerHTML += newsItemTemplate({img, content: text});
+                })
+            }
+            console.log(data)
+        })
+        .catch(err => console.log(err.message));
 })()
